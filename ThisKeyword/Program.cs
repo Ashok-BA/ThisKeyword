19c2abde15c87601656cc7d7a264ac529f9ca5aa
ashok <ashok@ASHOK>
2026-05-21 10:34:40 +0530

    Add project files.

---
+++ ThisKeyword/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisKeyword
+{
+    class newclass
+    {
+        int a;
+        int b;
+
+        public newclass(int a, int b) //Local Variables
+        {
+            this.a = a; //Instance variables = Local Variables
+            this.b = b;
+        }
+
+        public void add()
+        {
+            int c = a + b;
+            Console.WriteLine("sum is:" + c);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            newclass obj = new newclass(10,20);
+            obj.add();
+        }
+    }
+}
