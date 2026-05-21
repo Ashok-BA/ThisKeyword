6427a5a33377a5be642d35bc63bda96c68503706
Ashok-BA <ashok972001@outlook.com>
2026-05-21 12:22:02 +0530

    code updated

--- ThisKeyword/ConmstructorOverloading.cs
+++ ThisKeyword/ConmstructorOverloading.cs
@@ -1,61 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace ThisKeyword
-{
-    class student
-    {
-        int sid;
-        string sname;
+//namespace ThisKeyword
+//{
+//    class student
+//    {
+//        int sid;
+//        string sname;
 
-        public void studentdetails()
-        {
-            sid = 8;
-            sname = "ashok";
-        }
-        public student(int Sid, string Sname)
-        {
-            sid = Sid;
-            sname = Sname;
-        }
+//        public void studentdetails()
+//        {
+//            sid = 8;
+//            sname = "ashok";
+//        }
+//        public student(int Sid, string Sname)
+//        {
+//            sid = Sid;
+//            sname = Sname;
+//        }
 
-        public student(int sid)
-        {
-            this.sid = sid;
-            sname = "Not provided";
-        }
+//        public student(int sid)
+//        {
+//            this.sid = sid;
+//            sname = "Not provided";
+//        }
 
-        public student(string sname)
-        {
+//        public student(string sname)
+//        {
             
-            this.sname = sname;
+//            this.sname = sname;
 
-        }
+//        }
 
 
 
-        public void Displaydetails()
-        {
-            Console.WriteLine("Student ID is:" + sid);
-            Console.WriteLine("student name is:" + sname);
-        }
-    }
+//        public void Displaydetails()
+//        {
+//            Console.WriteLine("Student ID is:" + sid);
+//            Console.WriteLine("student name is:" + sname);
+//        }
+//    }
        
-    internal class ConmstructorOverloading
-    {
-        static void Main(string[] args)
-        {
-            student stud = new student(10,"AB");
-            stud.Displaydetails();
+//    internal class ConmstructorOverloading
+//    {
+//        static void Main(string[] args)
+//        {
+//            student stud = new student(10,"AB");
+//            stud.Displaydetails();
 
-            student stud1 = new student(10);
-            stud1.Displaydetails();
+//            student stud1 = new student(10);
+//            stud1.Displaydetails();
 
-            student stud2 = new student("Ashok");
-            stud2.Displaydetails();
-        }
-    }
-}
+//            student stud2 = new student("Ashok");
+//            stud2.Displaydetails();
+//        }
+//    }
+//}
---
+++ ThisKeyword/StudentDetailsUsingArray.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisKeyword
+{
+    class student
+    {
+        int id;
+        string name;
+
+        public student(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public void displaydetails()
+        {
+            Console.WriteLine("Student ID is:" + id);
+            Console.WriteLine("student Name is:" + name);
+            Console.WriteLine("------------------------");
+        }
+    }
+
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of students:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            student[] stud = new student[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter Student Id:");
+                int id = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter Student Name:");
+                string name = Console.ReadLine();
+
+                stud[i] = new student(id, name);
+            }
+
+            Console.WriteLine("studetns Details are:-");
+            for (int i = 0; i < stud.Length; i++)
+            {
+                stud[i].displaydetails();
+
+            }
+        }
+    }
+}
