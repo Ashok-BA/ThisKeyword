c87be3bb5568a06cfb59034db2eccc55ba49409f
Ashok-BA <ashok972001@outlook.com>
2026-05-21 11:24:55 +0530

    code updated

---
+++ ThisKeyword/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisKeyword
+{
+    class student
+    {
+        int id;
+        string sname;
+
+        public student(int id, string sname)
+        {
+            this.id = id;
+            this.sname = sname;
+        }
+
+        public void displaydetails()
+        {
+            Console.WriteLine("student Id is:" + id);
+            Console.WriteLine("Student Name is:" + sname);
+        }
+    }
+
+    internal class Student
+    {
+        static void Main(string[] args)
+        {
+            student std = new student(8,"ashok");
+            std.displaydetails();
+        }
+    }
+}
